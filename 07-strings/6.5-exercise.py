45226f31ec4d6c2a1c702865c61a82feb7a72b50
Saikat Das <saikatdas0790@gmail.com>
2021-01-11 09:53:24 +0530

    strings

---
+++ 07-strings/6.5-exercise.py
@@ -0,0 +1,14 @@
+# Write code using find() and string slicing (see section 6.10) to extract the number at the end of the line below. Convert the extracted value to a floating point number and print it out.
+
+text = "X-DSPAM-Confidence:    0.8475"
+
+spaceFoundPosition = text.find(" ")
+
+while text[spaceFoundPosition] == " ":
+    spaceFoundPosition += 1
+
+obtainedNumber = text[spaceFoundPosition:]
+
+output = float(obtainedNumber)
+
+print(output)
